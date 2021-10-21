d65e88b3c27d1b70f1e648cef04439d80eb55d4b
Afifafa <92510118+Afifafa@users.noreply.github.com>
2021-10-21 16:28:05 +0700

    Add

---
+++ src/modul3/percobaan/Tes.java
@@ -0,0 +1,9 @@
+package modul3.percobaan;
+
+public class Tes {
+    public static void main(String[] args) {
+        Rumah r = new Rumah();
+        r.setAlamat("Alamat saya di Jalan Pramuka");
+        System.out.println(r.alamat_rumah);
+    }
+}
