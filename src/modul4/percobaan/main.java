e6e756cdd450c22eaff34e1d321e2a16e795b093
Afifafa <92510118+Afifafa@users.noreply.github.com>
2021-10-30 22:18:53 +0700

    Add

---
+++ src/modul4/percobaan/main.java
@@ -0,0 +1,11 @@
+package modul4.percobaan;
+
+public class main {
+    public static void main(String[] args) {
+        Tabungan saya = new Tabungan("Fafa",  504104420, 1000000, 12345);
+        System.out.println("Nama \t : " + saya.getNama());
+        System.out.println("No Rekening \t : " + saya.getNoRekening());
+        System.out.println("Saldo \t : " + saya.getSaldo());
+        System.out.println("Pin \t : " + saya.getPin());
+    }
+}
