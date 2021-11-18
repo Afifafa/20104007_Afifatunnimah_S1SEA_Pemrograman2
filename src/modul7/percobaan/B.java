c5e2967aee018762321bfdcf7bb43bfd0dbea82a
Afifafa <92510118+Afifafa@users.noreply.github.com>
2021-11-18 14:17:44 +0700

    Add

---
+++ src/modul7/percobaan/B.java
@@ -0,0 +1,5 @@
+package modul7.percobaan;
+
+public class B extends A {
+
+}
---
+++ src/modul7/percobaan/Overload.java
@@ -0,0 +1,20 @@
+package modul7.percobaan;
+
+public class Overload {
+    void myOverload(A a){
+        System.out.println("Overload.myOverload(A a");
+    }
+
+    void myOverload(B b){
+        System.out.println("Overload.myOverload(B b");
+    }
+
+    public static void main(String[] args) {
+        Overload o = new Overload();
+        A a = new A();
+        C c = new C();
+
+        o.myOverload(a);
+        o.myOverload(c);
+    }
+}
